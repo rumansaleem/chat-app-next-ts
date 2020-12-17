e03d36c3472054f8f72c164cd283acd7d8eefca3
Ruman Saleem <ruman.saleem@jeevansathi.com>
2020-12-17 17:03:35 +0530

    non-floating  sidebar on widescreen & new msg fixd bottm

--- src/components/chat/ChatPage.tsx
+++ src/components/chat/ChatPage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import ChatSidebar from './ChatSidebar';
 import useScreenDimensions from '../../utils/useScreenDimensions';
-import { RouteInfo, RouterPageProps, SwitchRoutes } from '../../routes';
+import { RouterPageProps, SwitchRoutes } from '../../routes';
 import ChatHeader from './ChatHeader';
 import Message from './Message';
 import { CgAirplane, CgAttachment } from 'react-icons/cg';
@@ -44,24 +44,29 @@ const ChatPage: React.FC<RouterPageProps> = ({ routes }) => {
         <ChatSidebar
           id="chat-sidebar"
           hideSidebar={(): void => setShowSidebar(false)}
-          className="fixed z-40 w-sidebar bg-white text-black shadow h-full transform transition-all duration-300"
+          className={
+            (isWideScreen ? '' : 'fixed ') +
+            'z-40 w-sidebar bg-white text-black shadow h-full transform transition-all duration-300'
+          }
         />
       </CSSTransition>
-      <div id="chat-area" className="flex-1 flex flex-col">
+      <div id="chat-area" className="relative flex-1 flex flex-col">
         <ChatHeader chat={chat} showSidebarToggle={true} toggleSidebar={toggleSidebar} />
         <main
           id="message-container"
-          className="my-2 px-4 py-2 flex-1 flex flex-col-reverse space-y-4 space-y-reverse overflow-auto"
+          className="my-2 px-4 pt-2 pb-16 flex-1 flex flex-col-reverse overflow-auto"
         >
-          {chat ? (
-            chat.messages.map((message) => <Message key={message.id} message={message} />)
-          ) : chatId !== '' ? (
-            <p>Invalid Chat Selected, No Such Chat.</p>
-          ) : (
-            <p>Select a conversation from Sidebar</p>
-          )}
+          <div className="w-full max-w-screen-lg mx-auto flex flex-col-reverse space-y-4 space-y-reverse">
+            {chat ? (
+              chat.messages.map((message) => <Message key={message.id} message={message} />)
+            ) : chatId !== '' ? (
+              <p>Invalid Chat Selected, No Such Chat.</p>
+            ) : (
+              <p>Select a conversation from Sidebar</p>
+            )}
+          </div>
         </main>
-        <footer id="compose-message" className="p-4 pt-0">
+        <footer id="compose-message" className="absolute bg-gray-100 bottom-0 inset-x-0 p-4 pt-2">
           <div className="flex items-center space-x-2 rounded shadow-md bg-white px-2">
             <button className="p-2 rounded-full hover:bg-gray-200 text-gray-800">
               <CgAttachment className="w-4 h-4" />
